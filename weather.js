cee4c089ea773b8c3013a9a374b5f1b96e6af1e2
kbutz <kylebutz@gmail.com>
2016-03-27 22:57:50 -0500

    CSS changes, js clean-up, UI changes

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@
 // http://www.w3schools.com/jquery/ajax_getjson.asp
 // https://www.smashingmagazine.com/2012/02/beginners-guide-jquery-based-json-api-clients/
 // http://themarklee.com/2014/04/03/pulling-json-data-open-data-api/
-(function ($) {
+$( document ).ready(function() {
     // Calls function when the <button> is clicked
     $('button').on('click', function () {
         // remove resultset if this has already been run
@@ -17,13 +17,15 @@
         // chooses the largest city and displays that weather data.
         var zip = $('#zip').val();
         var country = $('#country').val();
-        var units = $('select option:selected').text();
+        var units = $('#units').val();
+        var catgif;
 
     if (zip && country)
     {
         // make AJAX call, storing the JSON output in data
-        $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + zip + ',' + country + ',za&units=' + units + '&APPID={YOURAPPID}&mode=json', function (data) {
+        $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + zip + ',' + country + ',za&units=' + units + '&APPID=0117bf81e4cbdc2ac2892799d1392ee4&mode=json', function (data) {
             
+            // Checks for imperial vs. metric and prints
             $(".results").append('<h3>' + data.name + ', ' + data.sys.country + '</h3>');
             if (units == "imperial"){
                 $(".results").append('<p><span class="conditionTitle">Current temperature:</span><span class="condition"> ' + data.main.temp + ' &#176;F</span><span></p>');
@@ -31,51 +33,46 @@
             else {
                 $(".results").append('<p><span class="conditionTitle">Current temperature:</span><span class="condition"> ' + data.main.temp + ' &#176;C</span><span></p>');
             }
-            
+
+            // Prints weather data
+            $(".results").append('<p>Weather description: ' + data.weather[0].description + '.</p>');
             
             //Check weather code for general description of weather using OpenWeatherMap's weather codes.
             // One of the JSON sections is an array, thus using array notation for weather[]
             // Blue skies, cat from http://giphy.com/gifs/26yiIkCrJxAqY
             if (data.weather[0].id === 800 || (data.weather[0].id <= 955 && data.weather[0].id >=951)){
-                $(".results").append('<p>Weather description: ' + data.weather[0].description + '.</p>');
-                $(".results").append('<div class="resultcat"><img src="http://www.kylebutz.com/weathercat/suncat.gif" class="resultcat"/></div>');
+                catgif = 'suncat.gif';
             }
             // light clouds, cat from: http://giphy.com/gifs/cH5VGNHfv46vS
             else if (data.weather[0].id <= 804 && data.weather[0].id >= 801){
-                $(".results").append('<p>Weather description: ' + data.weather[0].description + '.</p>');
-                $(".results").append('<div class="resultcat"><img src="http://www.kylebutz.com/weathercat/cloudcat.gif" class="resultcat"/></div>');
-
+                catgif = 'cloudcat.gif';
             }
             //severe or strange atmospheric conditions, cats from: http://giphy.com/gifs/halloween-betty-boop-boops-party-vXPvgpgPoKTRu
             else if (data.weather[0].id <= 781 && data.weather[0].id >= 730){
-                $(".results").append('<p>Weather description: ' + data.weather[0].description + '.</p>');
-                $(".results").append('<div class="resultcat"><img src="http://www.kylebutz.com/weathercat/strangecat.gif" class="resultcat"/></div>');
+                catgif = 'strangecat.gif';
             }
             //mist or haze
             else if (data.weather[0].id == 701 || data.weather[0].id == 721 || data.weather[0].id == 711){
-                $(".results").append('<p>Weather description: ' + data.weather[0].description + '.</p>');
-                $(".results").append('<div class="resultcat"><img src="http://www.kylebutz.com/weathercat/fogcat.jpg" width="500px" class="resultcat"/></div>');
+                catgif = 'fogcat.jpg';
             }
             // snow, cat from http://giphy.com/gifs/wUgWRubJHS7Ac
             else if (data.weather[0].id <= 622 && data.weather[0].id >= 600){
-                $(".results").append('<p>Weather description: ' + data.weather[0].description + '.</p>');
-                $(".results").append('<div class="resultcat"><img src="http://www.kylebutz.com/weathercat/snowcat.gif" class="resultcat"/></div>');
+                catgif = 'snowcat.gif';
             }
             // rain, cat from http://giphy.com/gifs/cat-animal-weather-h5Bgk3GwjdDUc
             else if (data.weather[0].id <= 531 && data.weather[0].id >= 500){
-                $(".results").append('<p>Weather description: ' + data.weather[0].description + '.</p>');
-                $(".results").append('<div class="resultcat"><img src="http://www.kylebutz.com/weathercat/raincat.gif" class="resultcat"/></div>');
+                catgif = 'raincat.gif';
             }
             // drizzle, cat from: http://theverybesttop10.com/cats-avoiding-the-rain/raincat
             else if (data.weather[0].id <= 321 && data.weather[0].id >= 300){
-                $(".results").append('<p>Weather description: ' + data.weather[0].description + '.</p>');
-                $(".results").append('<div class="resultcat"><img src="http://www.kylebutz.com/weathercat/drizzlecat.jpg" class="resultcat"/></div>');
+                catgif = 'drizzlecat.jpg';
             }
             // thunderstorm
             else {
-                $(".results").append('<p>Weather description: ' + data.weather[0].description + '.</p>');
-                $(".results").append('<div class="resultcat"><img src="http://www.kylebutz.com/weathercat/wetcat.jpg" class="resultcat"/></div>');
+                catgif = 'wetcat.jpg';
             }
+            // Prints catgif
+            $(".results").append('<div class="resultcat"><img src="http://www.kylebutz.com/weathercat/' + catgif +'" class="resultcat"/></div>');
         });
     }
     else if (zip && !country){
